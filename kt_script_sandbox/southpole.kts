37bcf371bed6ebb76470ebc4d940dd1ecda453b1
Chee Wai Wong <academicsnotes@gmail.com>
2025-01-28 00:41:24 +0800

    Commit scripts

---
+++ kt_script_sandbox/southpole.kts
@@ -0,0 +1,13 @@
+/***
+ * kotlinc -script southpole.kts
+ */
+
+ import java.time.*
+
+ val instant = Instant.now()
+ val southPole = instant.atZone(ZoneId.of("Antarctica/South_Pole"))
+ val dst = southPole.zone.rules.isDaylightSavings(instant)
+ 
+ 
+ println("It is ${southPole.toLocalTime()} (UTC${southPole.offset}) at the South Pole")
+ println("The south Pole ${if (dst) "is" else "is not"} on Daylight Savings Time")
---
+++ readme.md
@@ -0,0 +1,2 @@
+Upload GIT Project</BR>
+https://eclipsesource.com/blogs/2019/11/07/how-to-use-git-in-the-eclipse-theia-ide/</BR>
